e302c273bf10e8a8d228fb07361a04f6693160b0
sohan-shingade <sohanshingade@gmail.com>
2020-10-20 22:45:52 -0700

    Updates

---
+++ precache-manifest.511adaaf231504b4b4a91691e5d26f98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd0e6a79ba8b4aa7979bce6732e6a07",
+    "url": "/MVRT-Firebase-SideProject/index.html"
+  },
+  {
+    "revision": "b9225bd29b1118ab7c6b",
+    "url": "/MVRT-Firebase-SideProject/static/css/main.a72963eb.chunk.css"
+  },
+  {
+    "revision": "da344676a472f0ee47f1",
+    "url": "/MVRT-Firebase-SideProject/static/js/2.e2621ca1.chunk.js"
+  },
+  {
+    "revision": "2d54400cfeffd5a03c97473febb60b1e",
+    "url": "/MVRT-Firebase-SideProject/static/js/2.e2621ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9225bd29b1118ab7c6b",
+    "url": "/MVRT-Firebase-SideProject/static/js/main.1c23435d.chunk.js"
+  },
+  {
+    "revision": "472f39f9a2343bac29c6",
+    "url": "/MVRT-Firebase-SideProject/static/js/runtime-main.a4bef35b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MVRT-Firebase-SideProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
